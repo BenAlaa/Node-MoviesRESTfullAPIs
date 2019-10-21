231339c830fde5e979851f3752d4ad9fb36a4087
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2019-10-21 23:56:04 +0200

    Refactoring Router

---
+++ index.js
@@ -0,0 +1,18 @@
+ /**
+ * @author BenAlaa
+ * 
+ */
+
+
+
+//// joi is third party libary using for validate requests
+const Joi = require('@hapi/joi');
+const genres = require('./routes/genres');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use('/api/genres', genres);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
