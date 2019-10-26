9d61e3553763118ca6fa2d95fc0aa762d8207198
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2019-10-26 03:26:15 +0200

    link genres to mongoDB

--- index.js
+++ index.js
@@ -6,11 +6,15 @@
 
 
 //// joi is third party libary using for validate requests
-const Joi = require('@hapi/joi');
+
 const genres = require('./routes/genres');
 const express = require('express');
-const app = express();
-
+const app = express(); 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/vidly')
+ .then(() => console.log('Connected to MongoDB...'))
+ .catch((err) => console.log('Could not connect to MongoDB...',err));
+ 
 app.use(express.json());
 app.use('/api/genres', genres);
 
